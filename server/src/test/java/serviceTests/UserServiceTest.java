d273bd2f55992e17d357812a10827955db8db937
David Higueros <davhig22@byu.edu>
2024-02-23 15:55:06 -0700

    Added User Service Unit Tests

--- server/src/test/java/serviceTests/UserServiceTest.java
+++ server/src/test/java/serviceTests/UserServiceTest.java
@@ -1,7 +1,108 @@
 package serviceTests;
 
+import model.AuthData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.AppService;
+import service.UserService;
+import service.request.AuthRequest;
+import service.request.UserEnterRequest;
+import service.result.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
 
+    String username = "davhig22";
+    String password = "pass123";
+    String email = "davhig22@byu.edu";
+    String wrongUsername = "dabhig23";
+    String wrongPassword = "shall-not-pass";
+    UserEnterRequest enterRequest = new UserEnterRequest(username, password, email);
+    AuthRequest authRequest;
+
+    @BeforeEach
+    public void setUp() throws ServiceException {
+        AppService.clearData();
+    }
+
+    @Test
+    public void registerTest() throws ServiceException {
+        UserEnterResponse response = UserService.register(enterRequest);
+        Assertions.assertEquals(username, response.username());
+        Assertions.assertNotNull(response.authToken());
+    }
+
+    @Test
+    public void registerFail() throws ServiceException {
+        UserEnterRequest badUsername = new UserEnterRequest("", password, email);
+        Assertions.assertThrows(BadRequestException.class, () -> UserService.register(badUsername));
+
+        UserEnterRequest badPassword = new UserEnterRequest(username, "", email);
+        Assertions.assertThrows(BadRequestException.class, () -> UserService.register(badPassword));
+
+        UserEnterRequest badEmail = new UserEnterRequest(username, password, "");
+        Assertions.assertThrows(BadRequestException.class, () -> UserService.register(badEmail));
+
+        UserService.register(enterRequest);
+        Assertions.assertThrows(PreexistingException.class, () -> UserService.register(enterRequest));
+    }
+
+    @Test
+    public void loginTest() throws ServiceException {
+        UserService.register(enterRequest);
+        Assertions.assertDoesNotThrow(() -> UserService.login(enterRequest));
+
+        UserEnterResponse response = UserService.login(enterRequest);
+        Assertions.assertEquals(username, response.username());
+        Assertions.assertNotNull(response.authToken());
+    }
+
+    @Test
+    public void loginFail() throws ServiceException {
+        Assertions.assertThrows(UnauthorizedException.class, () -> UserService.login(enterRequest));
+
+        UserService.register(enterRequest);
+
+        UserEnterRequest wrongUser = new UserEnterRequest(wrongUsername, password, null);
+        Assertions.assertThrows(UnauthorizedException.class, () -> UserService.login(wrongUser));
+
+        UserEnterRequest wrongPass = new UserEnterRequest(username, wrongPassword, null);
+        Assertions.assertThrows(UnauthorizedException.class, () -> UserService.login(wrongPass));
+    }
+
+    @Test
+    public void logoutTest() throws ServiceException {
+        authRequest = new AuthRequest(UserService.register(enterRequest).authToken());
+        Assertions.assertDoesNotThrow(() -> UserService.logout(authRequest));
+
+        authRequest = new AuthRequest(UserService.login(enterRequest).authToken());
+        Assertions.assertDoesNotThrow(() -> UserService.logout(authRequest));
+    }
+
+    @Test
+    public void logoutFail() throws ServiceException {
+        authRequest = new AuthRequest("not-an-auth-token");
+        Assertions.assertThrows(UnauthorizedException.class, () -> UserService.logout(authRequest));
+
+        authRequest = new AuthRequest(UserService.register(enterRequest).authToken());
+        UserService.logout(authRequest);
+        Assertions.assertThrows(UnauthorizedException.class, () -> UserService.logout(authRequest));
+    }
+
+    @Test
+    public void validUserTest() throws ServiceException {
+        String authToken = "non-existent";
+        AuthData auth = UserService.validUser(authToken);
+
+        Assertions.assertNull(auth);
+
+        authToken = UserService.register(enterRequest).authToken();
+        auth = UserService.validUser(authToken);
+
+        Assertions.assertNotNull(auth);
+        Assertions.assertEquals(username, auth.username());
+        Assertions.assertNotNull(auth.authToken());
+    }
 }
