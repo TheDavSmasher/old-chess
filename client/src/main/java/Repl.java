04dccb308bf86d0f8df87a11e0019b7e60c118e8
David Higueros <davhig22@byu.edu>
2024-03-12 17:31:36 -0600

    Added basic repl funcionality

--- client/src/main/java/Repl.java
+++ client/src/main/java/Repl.java
@@ -1,18 +1,31 @@
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 import static ui.EscapeSequences.*;
 public class Repl {
 
-    public String run() {
+    public void run() {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        out.println("Intro");
+        out.print("Options");
 
-        ChessClient.run(out);
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt(out);
+            String line = scanner.nextLine();
 
-        return "";
+            try {
+                result = ChessClient.evaluate(line);
+            } catch (Throwable e) {
+                out.print(e);
+            }
+        }
+        out.println();
     }
 
     private void printPrompt(PrintStream out) {
-        out.print("\n" + UNSET_TEXT_COLOR + ">> " + SET_TEXT_COLOR_GREEN);
+        out.print("\n" + UNSET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_GREEN);
     }
 }
