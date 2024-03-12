a75facfad65d501d03c3126b0574e27417999aa3
David Higueros <davhig22@byu.edu>
2024-03-12 17:11:16 -0600

    Creation to handle command line

--- client/src/main/java/Repl.java
+++ client/src/main/java/Repl.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Repl {
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static ui.EscapeSequences.*;
+public class Repl {
+
+    public String evaluate() {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+        ChessClient.run(out);
+
+        return "";
+    }
+
+    private void printPrompt(PrintStream out) {
+        out.print("\n" + UNSET_TEXT_COLOR + ">> " + SET_TEXT_COLOR_GREEN);
+    }
 }
