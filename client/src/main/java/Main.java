7861535746231de9c261938825cd86a729705502
David Higueros <davhig22@byu.edu>
2024-03-12 10:41:29 -0600

    Initial additions for phase

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,16 @@
 import chess.*;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import static ui.EscapeSequences.*;
+
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+        out.print(ERASE_SCREEN);
     }
 }
