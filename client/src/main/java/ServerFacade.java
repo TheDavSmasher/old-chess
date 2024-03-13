9b6f9f3b6e810918e5d69c22d58b7015d7c3a575
David Higueros <davhig22@byu.edu>
2024-03-12 20:25:05 -0600

    Added method stubs for later implementation with server

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,2 +1,34 @@
+import model.*;
+
+import java.util.ArrayList;
+
 public class ServerFacade {
+
+    private final int port;
+
+    public ServerFacade(int port) {
+        this.port = port;
+    }
+
+    public AuthData register(String username, String password, String email) {
+        return null;
+    }
+
+    public AuthData login(String username, String password) {
+        return null;
+    }
+
+    public ArrayList<GameData> listGames(String authToken) {
+        return null;
+    }
+
+    public int createGame(String authToken, String gameName) {
+        return 0;
+    }
+
+    public void observeGame(String authToken, int gameID) {}
+
+    public void joinGame(String authToken, String color, int gameID) {}
+
+    public void logout(String authToken) {}
 }
