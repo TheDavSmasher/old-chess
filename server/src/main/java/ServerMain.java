b7f78fb1fd57680656b129b345bb63b7310bf6ab
David Higueros <davhig22@byu.edu>
2024-03-14 15:10:55 -0600

    Class Rename

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -1,7 +1,8 @@
 import chess.*;
 import server.ChessServer;
 
-public class Main {
+public class ServerMain {
+
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
