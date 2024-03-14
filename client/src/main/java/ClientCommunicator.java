6e3467a69d6b3cd2e37e03435fbf893e44095d64
David Higueros <davhig22@byu.edu>
2024-03-14 13:57:57 -0600

    Minor refactoring + throw exception on not 200

--- client/src/main/java/ClientCommunicator.java
+++ client/src/main/java/ClientCommunicator.java
@@ -1,31 +1,29 @@
 import com.google.gson.Gson;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URI;
-import java.util.Map;
 
 public class ClientCommunicator {
-    public static Object doPost(String urlString, String body, String authToken) throws IOException {
-        return doServerMethod(getConnection(urlString), "POST", body, authToken);
+    public static <T> T doPost(String urlString, String body, String authToken, Class<T> responseClass) throws IOException {
+        return doServerMethod(urlString, "POST", body, authToken, responseClass);
     }
 
-    public static Object doDelete(String urlString, String authToken) throws IOException {
-        return doServerMethod(getConnection(urlString), "DELETE", null, authToken);
+    public static <T> void doDelete(String urlString, String authToken, Class<T> responseClass) throws IOException {
+        doServerMethod(urlString, "DELETE", null, authToken, responseClass);
     }
 
-    public static Object doPut(String urlString, String body, String authToken) throws IOException {
-        return doServerMethod(getConnection(urlString), "PUT", body, authToken);
+    public static <T> void doPut(String urlString, String body, String authToken, Class<T> responseClass) throws IOException {
+        doServerMethod(urlString, "PUT", body, authToken, responseClass);
     }
 
-    public static Object doGet(String urlString, String authToken) throws IOException {
-        return doServerMethod(getConnection(urlString), "GET", null, authToken);
+    public static <T> T doGet(String urlString, String authToken, Class<T> responseClass) throws IOException {
+        return doServerMethod(urlString, "GET", null, authToken, responseClass);
     }
 
-    private static Object doServerMethod(HttpURLConnection connection, String method, String body, String authToken) throws IOException {
+    private static <T> T doServerMethod(String url, String method, String body, String authToken, Class<T> responseClass) throws IOException {
+        HttpURLConnection connection = getConnection(url);
+
         connection.setConnectTimeout(5000);
         connection.setRequestMethod(method);
         if (authToken != null) {
@@ -44,13 +42,15 @@ public class ClientCommunicator {
         if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
             try (InputStream responseBody = connection.getInputStream()) {
                 reader = new InputStreamReader(responseBody);
+                return new Gson().fromJson(reader, responseClass);
             }
         } else {
             try (InputStream responseBody = connection.getErrorStream()) {
                 reader = new InputStreamReader(responseBody);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                throw new IOException(bufferedReader.readLine());
             }
         }
-        return new Gson().fromJson(reader, Map.class);
     }
 
     private static HttpURLConnection getConnection(String urlString) throws IOException {
