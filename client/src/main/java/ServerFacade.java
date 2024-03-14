94b15a530746b83b0f34fddde052a6f4ce5729f2
David Higueros <davhig22@byu.edu>
2024-03-14 10:07:25 -0600

    Made static

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -4,31 +4,25 @@ import java.util.ArrayList;
 
 public class ServerFacade {
 
-    private final int port;
-
-    public ServerFacade(int port) {
-        this.port = port;
-    }
-
-    public AuthData register(String username, String password, String email) {
+    public static AuthData register(String username, String password, String email) {
         return null;
     }
 
-    public AuthData login(String username, String password) {
+    public static AuthData login(String username, String password) {
         return null;
     }
 
-    public ArrayList<GameData> listGames(String authToken) {
+    public static ArrayList<GameData> listGames(String authToken) {
         return null;
     }
 
-    public int createGame(String authToken, String gameName) {
+    public static int createGame(String authToken, String gameName) {
         return 0;
     }
 
-    public void observeGame(String authToken, int gameID) {}
+    public static void observeGame(String authToken, int gameID) {}
 
-    public void joinGame(String authToken, String color, int gameID) {}
+    public static void joinGame(String authToken, String color, int gameID) {}
 
-    public void logout(String authToken) {}
+    public static void logout(String authToken) {}
 }
