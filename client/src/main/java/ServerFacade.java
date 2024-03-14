9ab15ef957601fcb9c3e4833c621b24b8e801aed
David Higueros <davhig22@byu.edu>
2024-03-14 13:58:48 -0600

    Added calls to ClientCommunicator

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,43 +1,57 @@
 import com.google.gson.Gson;
-import model.dataAccess.AuthData;
 import model.dataAccess.GameData;
-import model.dataAccess.UserData;
+import model.request.CreateGameRequest;
+import model.request.JoinGameRequest;
+import model.request.UserEnterRequest;
+import model.response.CreateGameResponse;
+import model.response.EmptyResponse;
+import model.response.ListGamesResponse;
+import model.response.UserEnterResponse;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class ServerFacade {
 
     private static final String urlPort = "http://localhost:8080/";
 
-    public static AuthData register(String username, String password, String email) {
-        String body = new Gson().toJson(new UserData(username, password, email));
-        var map = ClientCommunicator.doPost(urlPort + "user", body, null);
-
-        return null;
+    public static UserEnterResponse register(String username, String password, String email) throws IOException {
+        String url = urlPort + "user";
+        String body = new Gson().toJson(new UserEnterRequest(username, password, email));
+        return ClientCommunicator.doPost(url, body, null, UserEnterResponse.class);
     }
 
-    public static AuthData login(String username, String password) {
-        String body = new Gson().toJson(new HashMap<String, String>());
-
-        var map = ClientCommunicator.doPost(urlPort + "session", body, null);
-
-        return null;
+    public static UserEnterResponse login(String username, String password) throws IOException {
+        String url = urlPort + "session";
+        String body = new Gson().toJson(new UserEnterRequest(username, password, null));
+        return ClientCommunicator.doPost(url, body, null, UserEnterResponse.class);
     }
 
-    public static ArrayList<GameData> listGames(String authToken) {
-        var map = ClientCommunicator.doGet(urlPort + "game", authToken);
-        return null;
+    public static ArrayList<GameData> listGames(String authToken) throws IOException {
+        ListGamesResponse response = ClientCommunicator.doGet(urlPort + "game", authToken, ListGamesResponse.class);
+        return response.games();
     }
 
-    public static void createGame(String authToken, String gameName) {
-        String body = new Gson().toJson();
-        var map = ClientCommunicator.doPost(urlPort + "game", body, authToken);
+    public static CreateGameResponse createGame(String authToken, String gameName) throws IOException {
+        String url = urlPort + "game";
+        String body = new Gson().toJson(new CreateGameRequest(gameName));
+        return ClientCommunicator.doPost(url, body, authToken, CreateGameResponse.class);
     }
 
-    public static GameData observeGame(String authToken, int gameID) {return null;}
+    public static void observeGame(String authToken, int gameID) throws IOException {
+        String url = urlPort + "game";
+        String body = new Gson().toJson(new JoinGameRequest(null, gameID));
+        ClientCommunicator.doPut(url, body, authToken, EmptyResponse.class);
+    }
 
-    public static GameData joinGame(String authToken, String color, int gameID) {return null;}
+    public static void joinGame(String authToken, String color, int gameID) throws IOException {
+        String url = urlPort + "game";
+        String body = new Gson().toJson(new JoinGameRequest(color, gameID));
+        ClientCommunicator.doPut(url, body, authToken, EmptyResponse.class);
+    }
 
-    public static void logout(String authToken) {}
+    public static void logout(String authToken) throws IOException {
+        String url = urlPort + "session";
+        ClientCommunicator.doDelete(url, authToken, EmptyResponse.class);
+    }
 }
