16707492171ecf41d28217a666f6294e1269021f
David Higueros <davhig22@byu.edu>
2024-03-14 14:11:44 -0600

    Renamed Server refactor

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,11 +1,11 @@
 import chess.*;
-import server.Server;
+import server.ChessServer;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
-        Server server = new Server();
+        ChessServer server = new ChessServer();
         server.run(8080);
     }
 }
