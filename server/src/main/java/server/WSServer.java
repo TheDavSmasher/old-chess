a388e8005422dafed78251909f112b0d476e6fb7
David Higueros <davhig22@byu.edu>
2024-03-21 21:45:13 -0600

    Added basic onMessage code, functionality pending

--- server/src/main/java/server/WSServer.java
+++ server/src/main/java/server/WSServer.java
@@ -1,13 +1,42 @@
 package server;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import spark.Session;
-import spark.Spark;
+import webSocketMessages.userCommands.*;
 
 @WebSocket
 public class WSServer {
     @OnWebSocketMessage
     public void onMessage(Session session, String message) {
-        //TODO
+        //TODO type adapter
+        UserGameCommand gameCommand = new Gson().fromJson(message, UserGameCommand.class);
+        switch (gameCommand.getCommandType()){
+            case JOIN_PLAYER -> join((JoinObserverCommand) gameCommand);
+            case JOIN_OBSERVER -> observe((JoinObserverCommand) gameCommand);
+            case MAKE_MOVE -> move((MakeMoveCommand) gameCommand);
+            case LEAVE -> leave((LeaveCommand) gameCommand);
+            case RESIGN -> resign((ResignCommand) gameCommand);
+        }
+    }
+
+    private void join(JoinObserverCommand command) {
+        ;
+    }
+
+    private void observe(JoinObserverCommand command) {
+        ;
+    }
+
+    private void move(MakeMoveCommand command) {
+        ;
+    }
+
+    private void leave(LeaveCommand command) {
+        ;
+    }
+
+    private void resign(ResignCommand command) {
+        ;
     }
 }
