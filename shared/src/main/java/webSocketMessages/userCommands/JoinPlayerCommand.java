e74233fb4a369550365ea224d86bf0719dc2b4c0
David Higueros <davhig22@byu.edu>
2024-03-21 21:34:17 -0600

    Subclasses UserGameCommand

---
+++ shared/src/main/java/webSocketMessages/userCommands/JoinPlayerCommand.java
@@ -0,0 +1,21 @@
+package webSocketMessages.userCommands;
+
+import chess.ChessGame;
+
+public class JoinPlayerCommand extends UserGameCommand {
+    private final int gameID;
+    private final ChessGame.TeamColor color;
+    public JoinPlayerCommand(String authToken, int gameID, ChessGame.TeamColor playerColor) {
+        super(authToken);
+        this.gameID = gameID;
+        color = playerColor;
+    }
+
+    public ChessGame.TeamColor getColor() {
+        return color;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+}
