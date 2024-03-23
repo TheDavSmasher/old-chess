e327dcee31b23cf88d3a1a33a98db2564fa226fe
David Higueros <davhig22@byu.edu>
2024-03-23 11:17:00 -0600

    Added Session storing

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -2,25 +2,30 @@ package server.websocket;
 
 import org.eclipse.jetty.websocket.api.Session;
 
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
     private final ConcurrentHashMap<String, Connection> userConnections = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<String, Integer> connectionsToGames = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Integer, ArrayList<Connection>> connectionsToGames = new ConcurrentHashMap<>();
 
-    public void addToUsers(String username, Session session) {
-        userConnections.put(username, new Connection(username, session));
+    public void addToUsers(String authToken, String username, Session session) {
+        userConnections.put(authToken, new Connection(username, session));
     }
 
-    public void removeFromUser(String username) {
-        userConnections.remove(username);
+    public void removeFromUser(String authToken) {
+        userConnections.remove(authToken);
     }
 
-    public void addToGame(int gameID, String username) {
-        connectionsToGames.put(username, gameID);
+    public void addToGame(int gameID, Connection connection) {
+        ArrayList<Connection> oldConnections = connectionsToGames.get(gameID);
+        oldConnections.add(connection);
+        connectionsToGames.put(gameID, oldConnections);
     }
 
-    public void removeFromGame(String username) {
-        connectionsToGames.remove(username);
+    public void removeFromGame(int gameID, Connection connection) {
+        ArrayList<Connection> oldConnections = connectionsToGames.get(gameID);
+        oldConnections.remove(connection);
+        connectionsToGames.put(gameID, oldConnections);
     }
 }
