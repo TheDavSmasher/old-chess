843a57182c2bdd989185966ac6bface8cafd340e
David Higueros <davhig22@byu.edu>
2024-04-02 12:51:20 -0600

    Modified methods for easier access and storage

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -2,6 +2,7 @@ package server.websocket;
 
 import org.eclipse.jetty.websocket.api.Session;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -9,23 +10,34 @@ public class ConnectionManager {
     private final ConcurrentHashMap<String, Connection> userConnections = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<Integer, ArrayList<Connection>> connectionsToGames = new ConcurrentHashMap<>();
 
-    public void addToUsers(String authToken, String username, Session session) {
-        userConnections.put(authToken, new Connection(username, session));
+    private void addToUsers(String authToken, Connection connection) {
+        userConnections.put(authToken, connection);
     }
 
-    public void removeFromUser(String authToken) {
+    private void removeFromUser(String authToken) {
         userConnections.remove(authToken);
     }
 
-    public void addToGame(int gameID, Connection connection) {
+    public void addToGame(int gameID, String authToken, String username, Session session) {
+        Connection newConnection = new Connection(username, session);
         ArrayList<Connection> oldConnections = connectionsToGames.get(gameID);
-        oldConnections.add(connection);
+        oldConnections.add(newConnection);
         connectionsToGames.put(gameID, oldConnections);
+        addToUsers(authToken, newConnection);
     }
 
-    public void removeFromGame(int gameID, Connection connection) {
+    public void removeFromGame(int gameID, String authToken) {
         ArrayList<Connection> oldConnections = connectionsToGames.get(gameID);
-        oldConnections.remove(connection);
+        oldConnections.remove(getFromUsers(authToken));
         connectionsToGames.put(gameID, oldConnections);
+        removeFromUser(authToken);
+    }
+
+    public Connection getFromUsers(String authToken) { return userConnections.get(authToken); }
+
+    public void sendToConnection(Connection connection, String message) {
+        try {
+            connection.send(message);
+        } catch (IOException ignored) {}
     }
 }
