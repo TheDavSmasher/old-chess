fc46c6debc34739fdef8cddca58932c207c56b6b
David Higueros <davhig22@byu.edu>
2024-04-02 19:13:04 -0600

    Small refactoring for tests

--- shared/src/main/java/webSocketMessages/serverMessages/ErrorMessage.java
+++ shared/src/main/java/webSocketMessages/serverMessages/ErrorMessage.java
@@ -1,13 +1,13 @@
 package webSocketMessages.serverMessages;
 
 public class ErrorMessage extends ServerMessage {
-    private final String error;
+    private final String errorMessage;
     public ErrorMessage(String errorMessage) {
         super(ServerMessageType.ERROR);
-        error = errorMessage;
+        this.errorMessage = errorMessage;
     }
 
     public String getError() {
-        return error;
+        return errorMessage;
     }
 }
