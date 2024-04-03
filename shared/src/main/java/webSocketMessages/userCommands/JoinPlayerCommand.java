95237b36745bbc77fc8e5ce8ee9a363d0d4169e9
David Higueros <davhig22@byu.edu>
2024-04-02 21:00:04 -0600

    Small variable rename

--- shared/src/main/java/webSocketMessages/userCommands/JoinPlayerCommand.java
+++ shared/src/main/java/webSocketMessages/userCommands/JoinPlayerCommand.java
@@ -4,16 +4,16 @@ import chess.ChessGame;
 
 public class JoinPlayerCommand extends UserGameCommand {
     private final int gameID;
-    private final ChessGame.TeamColor color;
+    private final ChessGame.TeamColor playerColor;
     public JoinPlayerCommand(String authToken, int gameID, ChessGame.TeamColor playerColor) {
         super(authToken);
         this.commandType = CommandType.JOIN_PLAYER;
         this.gameID = gameID;
-        color = playerColor;
+        this.playerColor = playerColor;
     }
 
     public ChessGame.TeamColor getColor() {
-        return color;
+        return playerColor;
     }
 
     public int getGameID() {
