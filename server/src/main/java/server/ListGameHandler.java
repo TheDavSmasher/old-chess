92f06d5d6bdd542f21603086cc3838773d33459b
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-02-28 13:15:19 -0700

    Changed to use serialize method

--- server/src/main/java/server/ListGameHandler.java
+++ server/src/main/java/server/ListGameHandler.java
@@ -1,6 +1,5 @@
 package server;
 
-import com.google.gson.Gson;
 import service.GameService;
 import model.response.ListGamesResponse;
 import model.response.result.ServiceException;
@@ -10,6 +9,6 @@ public class ListGameHandler extends ObjectSerializer {
     @Override
     public String serviceHandle(Request request) throws ServiceException {
         ListGamesResponse listResponse = GameService.getAllGames(getAuthToken(request));
-        return new Gson().toJson(listResponse);
+        return serialize(listResponse);
     }
 }
